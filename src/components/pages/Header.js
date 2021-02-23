a445c729589813ee4491d77d24b74574267dd3eb
Syedzainjeelani <sayedzainjeelani@gmail.com>
2021-02-23 17:56:10 +0500

    Change my portfolio to developer tagline

--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -12,36 +12,37 @@ const Header = () => {
         })
     }
 
-    const toggleDarkMode = () =>  {
+    const toggleDarkMode = () => {
         document.documentElement.classList.toggle('dark-mode')
         document.getElementById('not-dark').classList.toggle('inverse-dark')
         document.getElementById('not').classList.toggle('inverse-dark')
         var x = document.getElementsByClassName('img-pro')
-        for(let i = 0; i < x.length; i += 1) {
-          x.item(i).classList.toggle("inverse-dark");
+        for (let i = 0; i < x.length; i += 1) {
+            x.item(i).classList.toggle("inverse-dark");
         }
-        
+
         if (document.documentElement.classList.contains('dark-mode'))
-          localStorage.setItem('mode', 'Dark')
+            localStorage.setItem('mode', 'Dark')
         else
-          localStorage.setItem('mode', 'Light')
-        }
+            localStorage.setItem('mode', 'Light')
+    }
 
     return (
         <div>
             <div className="Header">
-                <h1>{ `I'm ${header.name}` }</h1>
-            <p className="line-1 anim-typewriter">and this is my portfolio... </p>
-            <label className="switch">
-                <input id="mode-switch" onClick={toggleDarkMode} type="checkbox"/>
-                <span className="slider round"></span>
-            </label>
-            <HeaderButton/>
+                <h1>{`I'm ${header.name}`}</h1>
+                {/* <p className="line-1 anim-typewriter">and this is my portfolio... </p> */}
+                <p className="line-1 anim-typewriter">A Mobile & Web Developer... </p>
+                <label className="switch">
+                    <input id="mode-switch" onClick={toggleDarkMode} type="checkbox" />
+                    <span className="slider round"></span>
+                </label>
+                <HeaderButton />
             </div>
             <img id="not-dark" onClick={scrollTo} alt="Go to top" title="Go to top" className="gtp" src="profile.png"></img>
         </div>
     )
-    
+
 }
 
 export default Header;
